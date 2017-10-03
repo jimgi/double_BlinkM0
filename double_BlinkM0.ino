457e57fe3717eea7180bbad4fbd3f0d4ce758504
jimgi <jimgi@btinternet.com>
2017-10-03 22:13:45 +0100

    Reorganise loop and tidy up.
    Changed the loop structure to show a dot as activity.  Moved some housekeeping task into simple functions to aid legibility.

--- double_BlinkM0.ino
+++ double_BlinkM0.ino
@@ -1,23 +1,11 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  Test program for Adafruit Feather shields.
+  Uses mostly the Adafruit libraries.
+  See www.adafruit.com
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+  See aslo Radiohead libraries.
+  Currently only a recieve data node, for connecting to a Linux host or othewise.
   
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  
-  modified 8 Sep 2016
-  by Colby Newman
 */
 
 #include <Wire.h>
@@ -27,8 +15,6 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 #include <RHDatagram.h>  // Use the second Radiohead if you want to use a Manager.
-
-
  
 /* Interface for Feather m0 w/wing */
 #define RFM95_RST     11   // "A"
@@ -36,27 +22,34 @@
 #define RFM95_INT     6    // "D"
  
 
-#define RF95_FREQ 868.0 // Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 868.0 // Change to 868.0 or other frequency, must match RX's freq!
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 uint8_t len = sizeof(buf);
 
 // Singleton instance of the radio driver and also the radio manager.
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-RHDatagram datagram (RHGenericDriver &rf95, uint8_t thisAddress=0); 
+RHDatagram datagram (RHGenericDriver &rf95, uint8_t thisAddress=0);   // Use if you want to define nodes.
+
+
 
 #define VBATPIN A7
 float measuredvbat = analogRead(VBATPIN);
 
+// Instance of the precision real time clock (not M0 internal RTC)
 RTC_DS3231 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+DateTime now;
 
 
+/*********************************/
+/*           SETUP               */
+/*********************************/
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-
+  // Now the RTC
   Serial.begin(9600);
   delay(3000); // wait for console opening
   Serial.println("Feather LoRa TX and RTC Test!");
@@ -77,6 +70,7 @@ void setup() {
   
 /* Set thus line to specifically set the time. */
   // rtc.adjust(DateTime(2017, 9, 27, 23, 7, 0));
+
 
 // manual reset of LoRa
   digitalWrite(RFM95_RST, LOW);
@@ -103,7 +97,6 @@ void setup() {
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
-
  
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
@@ -111,33 +104,95 @@ int16_t packetnum = 0;  // packet counter, we increment per xmission
  }  // End of setup
 
 
-
+/*********************************/
+/*           MAIN                */
+/*********************************/
 
 // the loop function runs over and over again forever
 void loop() {
 
-measuredvbat *= 2;    // we divided by 2, so multiply back
-measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-measuredvbat /= 1024; // convert to voltage
-Serial.print("VBat: " ); Serial.println(measuredvbat);
 
 
- DateTime now = rtc.now();
+ 
+/* Wait for a message over Lora */
+
+//Serial.println("Waiting for data..."); delay(10);
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {      
+      Serial.println();
+      now = rtc.now();
+      showTime(now);      // Simple function to display datetime.
+      
+      Serial.println();
+       
+      Serial.print("Data received: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+      // do a double blink acknowledge
+      blink();
+
+/* Show the battery voltage */
+      measuredvbat *= 2;    // we divided by 2, so multiply back
+      measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+      measuredvbat /= 1024; // convert to voltage
+      Serial.print("VBat: " ); Serial.println(measuredvbat);
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.print(".");                  // Just show activity
+  }
+
     
+    delay(1000);
+
+
+}  // End of main
+
+
+
+/*********************************/
+/*           FUNCTIONS           */
+/*********************************/
+
+void blink () {
+   // do a double blink acknowledge  
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(100);                       // wait for a second
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      delay(100);                       // wait for a second
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(100);                       // wait for a second
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      delay(1000);                       // wait for a second
+}
+
+void showTime( DateTime now) {
+   
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(' ');
     Serial.print(now.day(), DEC);
     Serial.print('/');
     Serial.print(now.month(), DEC);
     Serial.print('/');
     Serial.print(now.year(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
+    Serial.print(' ');
     Serial.print(now.hour(), DEC);
     Serial.print(':');
     Serial.print(now.minute(), DEC);
     Serial.print(':');
     Serial.print(now.second(), DEC);
     Serial.println();
+
+  
 /*    
     Serial.print(" since midnight 1/1/1970 = ");
     Serial.print(now.unixtime());
@@ -161,44 +216,8 @@ Serial.print("VBat: " ); Serial.println(measuredvbat);
     Serial.print(':');
     Serial.print(future.second(), DEC);
  */
-    Serial.println();
-    Serial.println();
-    
-/* Wait for a message over Lora */
-
-Serial.println("Waiting for data..."); delay(10);
-  if (rf95.waitAvailableTimeout(1000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {
-      Serial.print("Data received: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-      // do a double blink acknowledge  
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(100);                       // wait for a second
-      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-      delay(100);                       // wait for a second
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(100);                       // wait for a second
-      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-      delay(1000);                       // wait for a second
-    
-    }
-    else
-    {
-      Serial.println("Receive failed");
-    }
-  }
-  else
-  {
-    Serial.print(".");                  // Just show activity
-  }
-
-    
-    delay(1000);
+}
 
 
-}  // End of main
+
+
