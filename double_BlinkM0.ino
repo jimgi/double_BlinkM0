2f9447ef1f647d8c0e260c7b4fb11d5ac745e4da
jimgi <jimgi@btinternet.com>
2018-01-10 20:22:48 +0000

    Integration of Feather M0 LoRA
    This file has changes to allow it to be used with the Adafruit Feather with an on-board LoRA device.  This is mounted on a Featherwing terminal board along with a precision real time clock module.

--- double_BlinkM0.ino
+++ double_BlinkM0.ino
@@ -10,16 +10,23 @@
 
 #include <Wire.h>
 #include "RTClib.h"
+#include <RTCZero.h>  // This libary for the M0 RTC?
 
 /* Set up the LoRa radio */ 
 #include <SPI.h>
 #include <RH_RF95.h>
 #include <RHDatagram.h>  // Use the second Radiohead if you want to use a Manager.
  
-/* Interface for Feather m0 w/wing */
+/* Interface for Feather m0 w/wing 
 #define RFM95_RST     11   // "A"
 #define RFM95_CS      10   // "B"
 #define RFM95_INT     6    // "D"
+*/
+ 
+/* Interface for feather m0   */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
  
 
 #define RF95_FREQ 868.0 // Change to 868.0 or other frequency, must match RX's freq!
@@ -28,7 +35,7 @@ uint8_t len = sizeof(buf);
 
 // Singleton instance of the radio driver and also the radio manager.
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-RHDatagram datagram (RHGenericDriver &rf95, uint8_t thisAddress=0);   // Use if you want to define nodes.
+//RHDatagram datagram (RHGenericDriver &rf95, uint8_t thisAddress=0);   // Use if you want to define nodes.
 
 
 
@@ -36,10 +43,11 @@ RHDatagram datagram (RHGenericDriver &rf95, uint8_t thisAddress=0);   // Use if 
 float measuredvbat = analogRead(VBATPIN);
 
 // Instance of the precision real time clock (not M0 internal RTC)
-RTC_DS3231 rtc;
+RTC_DS3231 rtc; // High precision feather RTC.
+RTCZero rtcB;   // An instance of the on-chip RTC
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 DateTime now;
-
+DateTime nowB;
 
 /*********************************/
 /*           SETUP               */
@@ -49,11 +57,15 @@ DateTime now;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  // Now the RTC
-  Serial.begin(9600);
+  pinMode(RFM95_RST, OUTPUT);
+ 
+  Serial.begin(115200);
   delay(3000); // wait for console opening
   Serial.println("Feather LoRa TX and RTC Test!");
-
+  
+   /* Now the RTC  */
+  //rtcB.begin();  // Initialise on-chip clock to 24 hour format
+  
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
@@ -67,9 +79,12 @@ void setup() {
     // January 21, 2014 at 3am you would call:
     //rtc.adjust(DateTime(2017, 9, 27, 23, 03, 0));
   }
-  
 /* Set thus line to specifically set the time. */
   // rtc.adjust(DateTime(2017, 9, 27, 23, 7, 0));
+
+/*  Use the high precision clock to set the on-chip clock  */  
+ rtcB.setDate(now.day(), now.month(), now.year());
+ rtcB.setTime(now.hour(), now.minute(), now.second());
 
 
 // manual reset of LoRa
@@ -83,6 +98,7 @@ void setup() {
     while (1);
   }
   Serial.println("LoRa radio init OK!");
+
  
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
@@ -90,14 +106,15 @@ void setup() {
     while (1);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
   
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
  
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
- 
+     rf95.setTxPower(23, false);
+     
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 
@@ -108,38 +125,41 @@ int16_t packetnum = 0;  // packet counter, we increment per xmission
 /*           MAIN                */
 /*********************************/
 
-// the loop function runs over and over again forever
 void loop() {
 
-
-
- 
 /* Wait for a message over Lora */
 
-//Serial.println("Waiting for data..."); delay(10);
+//Serial.println("Waiting for data..."); //delay(100);
   if (rf95.waitAvailableTimeout(1000))
+  //if (rf95.available())
   { 
     // Should be a reply message for us now   
     if (rf95.recv(buf, &len))
-   {      
-      Serial.println();
+   {    
+      //Serial.println();
       now = rtc.now();
-      showTime(now);      // Simple function to display datetime.
+      //showTime(now);      // Simple function to display datetime.
       
-      Serial.println();
+      //nowB = getRTCB();
+      //showTime(nowB);      // Simple function to display datetime from the on-chip clock.
+      
+      //Serial.println();
        
-      Serial.print("Data received: ");
-      Serial.println((char*)buf);
+      Serial.print("Data received on M0: ");
+      //Serial.println(RH_RF95_MAX_MESSAGE_LEN);
+    
+      Serial.print((char*)buf);
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
       // do a double blink acknowledge
       blink();
 
-/* Show the battery voltage */
+      /* Show the battery voltage */
       measuredvbat *= 2;    // we divided by 2, so multiply back
       measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
       measuredvbat /= 1024; // convert to voltage
-      Serial.print("VBat: " ); Serial.println(measuredvbat);
+      //Serial.print("VBat: " );    // Turn off for test!
+      //Serial.println(measuredvbat);
     }
     else
     {
@@ -148,11 +168,10 @@ void loop() {
   }
   else
   {
-    Serial.print(".");                  // Just show activity
-  }
-
-    
+    //Serial.print(".");                  // Just show activity
     delay(1000);
+    
+  }
 
 
 }  // End of main
@@ -175,21 +194,21 @@ void blink () {
       delay(1000);                       // wait for a second
 }
 
-void showTime( DateTime now) {
+void showTime( DateTime timeValue) {
    
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(daysOfTheWeek[timeValue.dayOfTheWeek()]);
     Serial.print(' ');
-    Serial.print(now.day(), DEC);
+    Serial.print(timeValue.day(), DEC);
     Serial.print('/');
-    Serial.print(now.month(), DEC);
+    Serial.print(timeValue.month(), DEC);
     Serial.print('/');
-    Serial.print(now.year(), DEC);
+    Serial.print(timeValue.year(), DEC);
     Serial.print(' ');
-    Serial.print(now.hour(), DEC);
+    Serial.print(timeValue.hour(), DEC);
     Serial.print(':');
-    Serial.print(now.minute(), DEC);
+    Serial.print(timeValue.minute(), DEC);
     Serial.print(':');
-    Serial.print(now.second(), DEC);
+    Serial.print(timeValue.second(), DEC);
     Serial.println();
 
   
@@ -216,6 +235,27 @@ void showTime( DateTime now) {
     Serial.print(':');
     Serial.print(future.second(), DEC);
  */
+}
+
+/* Get the time from the on-chip Real Time Clock  */
+DateTime getRTCB ()  {
+    DateTime rtcBTime = (
+      rtcB.getDay(),
+      rtcB.getMonth(),
+      rtcB.getYear(),
+      rtcB.getHours(),
+      rtcB.getMinutes(),
+      rtcB.getSeconds()
+    );
+}
+
+void clearBuf() {
+  int count =0;
+     while (count < RH_RF95_MAX_MESSAGE_LEN) {
+     buf[count]=0x00;
+     count++;
+    }
+    Serial.println();
 }
 
 
